2d3b9751ca558ec84819e03d7fea1f39d14d8294
weirdstoies <daniltatarinof@mail.ru>
2019-10-13 19:27:26 +0300

    csv converter realization

--- Lab2/lab2/src/main/java/com/company/Main.java
+++ Lab2/lab2/src/main/java/com/company/Main.java
@@ -1,7 +1,11 @@
 package com.company;
 
+import com.opencsv.CSVWriter;
 import sun.awt.geom.AreaOp;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Dictionary;
 
 public class Main {
@@ -18,15 +22,32 @@ public class Main {
 
 	    System system = new System(sinus,cosinus,cosecant,cotangent, secant, logarithm, naturalLogarithm);
 
-        double start = -10;
+        String csv = "data.csv";
+
+        double current = -10;
         double finish = 10;
         double step = 0.2;
+        try {
+            CSVWriter writer = new CSVWriter(new FileWriter(csv));
 
-//        Dictionary<double, double>
-//
-//        for(;start < finish; start += step){
-//
-//        }
+            for(;current < finish; current += step){
+                ArrayList<String> csvLine = new ArrayList<String>();
+
+                csvLine.add(Double.toString(current));
+                csvLine.add(Double.toString(sinus.sin(current)));
+                csvLine.add(Double.toString(cosinus.cos(current)));
+                csvLine.add(Double.toString(cotangent.ctg(current)));
+                csvLine.add(Double.toString(secant.sec(current)));
+                csvLine.add(Double.toString(cosecant.cosec(current)));
+                csvLine.add(Double.toString(naturalLogarithm.ln(current)));
+                csvLine.add(Double.toString(logarithm.log(current, 2)));
+
+                writer.writeNext(csvLine.toArray(new String[csvLine.size()]));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
 
--- Lab2/lab2/src/test/java/LogarithmTest.java
+++ Lab2/lab2/src/test/java/LogarithmTest.java
@@ -1,24 +1,24 @@
-//import net.jqwik.api.*;
-//
-//import java.util.List;
-//import com.company.Logarithm;
-//import org.assertj.core.data.Offset;
-//import org.assertj.core.data.Percentage;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//public class LogarithmTest {
-//
-//    @Provide
-//    Arbitrary<Integer> first(){
-//        return Arbitraries.integers().between(0, 300);
-//    }
-//
-//    @Property
-//    void log_check(@ForAll("first") Integer any_int){
-//        Logarithm ln = new Logarithm();
-//        double value = ln.ln(any_int);
-//        double pow = Math.pow(Math.E, value);
-//        assertThat(pow).isCloseTo(any_int, Offset.offset(0.001));
-//    }
-//}
+import net.jqwik.api.*;
+
+import java.util.List;
+import com.company.Logarithm;
+import org.assertj.core.data.Offset;
+import org.assertj.core.data.Percentage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LogarithmTest {
+
+    @Provide
+    Arbitrary<Integer> first(){
+        return Arbitraries.integers().between(0, 300);
+    }
+
+    @Property
+    void log_check(@ForAll("first") Integer any_int){
+        Logarithm ln = new Logarithm();
+        double value = ln.ln(any_int);
+        double pow = Math.pow(Math.E, value);
+        assertThat(pow).isCloseTo(any_int, Offset.offset(0.001));
+    }
+}
