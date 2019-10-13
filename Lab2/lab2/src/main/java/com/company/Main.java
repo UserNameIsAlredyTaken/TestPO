cc942d8c3224b9bebac4f6b9bbae15d62b333d43
weirdstoies <daniltatarinof@mail.ru>
2019-10-13 18:27:11 +0300

    sustem realization

--- Lab2/lab2/src/main/java/com/company/Main.java
+++ Lab2/lab2/src/main/java/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
+import sun.awt.geom.AreaOp;
+
+import java.util.Dictionary;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Sinus sinus = new Sinus();
+        Cosinus cosinus = new Cosinus(sinus);
+	    Cotangent cotangent = new Cotangent(cosinus, sinus);
+	    Secant secant = new Secant(cosinus);
+	    Cosecant cosecant = new Cosecant(sinus);
+
+	    NaturalLogarithm naturalLogarithm = new NaturalLogarithm();
+	    Logarithm logarithm = new Logarithm(naturalLogarithm);
+
+	    System system = new System(sinus,cosinus,cosecant,cotangent, secant, logarithm, naturalLogarithm);
+
+        double start = -10;
+        double finish = 10;
+        double step = 0.2;
+
+//        Dictionary<double, double>
+//
+//        for(;start < finish; start += step){
+//
+//        }
+
+
+
     }
 }
--- Lab2/lab2/src/main/java/com/company/System.java
+++ Lab2/lab2/src/main/java/com/company/System.java
@@ -8,9 +8,32 @@ public class System {
     Secant secant;
 
     Logarithm log;
+    NaturalLogarithm ln;
 
-    public System(){
+    public System(Sinus sin,
+            Cosinus cos,
+            Cosecant cosecant,
+            Cotangent cotangent,
+            Secant secant,
+            Logarithm log,
+            NaturalLogarithm ln){
+        this.sin = sin;
+        this.cos = cos;
+        this.cosecant = cosecant;
+        this.cotangent = cotangent;
+        this.secant = secant;
+        this.log = log;
+        this.ln = ln;
+    }
 
+    public double SysResult(double x){
+        if(x <= 0){
+            return ((cotangent.ctg(x) + secant.sec(x)) / secant.sec(x) - secant.sec(x)) *
+                    (Math.pow(Math.pow(sin.sin(x),3) + secant.sec(x) + sin.sin(x) + cosecant.cosec(x),3));
+        }else{
+            return Math.pow(Math.pow(log.log(x, 5),2), 2) - log.log(x, 2) - log.log(x, 2) +
+                    Math.pow(log.log(x, 5), 3) / (ln.ln(x) / Math.pow(log.log(x, 2), 3) + (log.log(x, 10) - log.log(x, 5)));
+        }
     }
 
 }
