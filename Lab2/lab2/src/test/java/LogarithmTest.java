10fa870510d410f3e0e78a84d80a001ffde7a524
SlyF0Xx <max.tolstukhin@mail.ru>
2019-10-15 22:06:44 +0300

    Add Log tests

---
+++ Lab2/lab2/src/test/java/LogarithmTest.java
@@ -0,0 +1,37 @@
+import com.company.Logarithm;
+import com.company.NaturalLogarithm;
+import net.jqwik.api.*;
+import org.assertj.core.data.Offset;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LogarithmTest {
+    @Provide
+    Arbitrary<Integer> first(){
+        return Arbitraries.integers().between(0, 300);
+    }
+
+    @Property
+    void log_5_check(@ForAll("first") Integer any_int){
+        Logarithm ln = new Logarithm(new NaturalLogarithm());
+        double value = ln.log(any_int, 5);
+        double pow = Math.pow(5, value);
+        assertThat(pow).isCloseTo(any_int, Offset.offset(0.001));
+    }
+
+    @Property
+    void log_2_check(@ForAll("first") Integer any_int){
+        Logarithm ln = new Logarithm(new NaturalLogarithm());
+        double value = ln.log(any_int, 2);
+        double pow = Math.pow(2, value);
+        assertThat(pow).isCloseTo(any_int, Offset.offset(0.001));
+    }
+
+    @Property
+    void log_10_check(@ForAll("first") Integer any_int){
+        Logarithm ln = new Logarithm(new NaturalLogarithm());
+        double value = ln.log(any_int, 10);
+        double pow = Math.pow(10, value);
+        assertThat(pow).isCloseTo(any_int, Offset.offset(0.001));
+    }
+}
