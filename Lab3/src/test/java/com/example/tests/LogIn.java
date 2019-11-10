e89ee8aa3f7f5b1bb5a29160621c1aba8599eff2
SlyF0Xx <max.tolstukhin@mail.ru>
2019-11-10 19:51:37 +0300

    Login test added

--- Lab3/src/test/java/com/example/tests/LogIn.java
+++ Lab3/src/test/java/com/example/tests/LogIn.java
@@ -6,6 +6,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -17,7 +18,7 @@ public class LogIn {
 
   @Before
   public void setUp() throws Exception {
-    driver = new FirefoxDriver();
+    driver = new ChromeDriver();
     baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
@@ -26,15 +27,24 @@ public class LogIn {
   public void testLogIn() throws Exception {
     driver.get("https://otvet.mail.ru/");
     driver.findElement(By.id("PH_authLink")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectFrame | index=1 | ]]
+    driver.switchTo().frame(1);
+
+    driver.findElement(By.name("Login")).sendKeys("LabTPO");
+    driver.findElement(By.xpath("(.//*[@data-test-id='next-button'])")).click();
+
     driver.findElement(By.name("Password")).clear();
-    driver.findElement(By.name("Password")).sendKeys("NewPass174");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Войти в аккаунт'])[1]/following::span[1]")).click();
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Сменить аккаунт'])[1]/following::span[1]")).click();
+    driver.findElement(By.name("Password")).sendKeys("testingpo3");
+
+    driver.findElement(By.xpath("(.//*[@data-test-id='submit-button'])")).click();
+
+    assertEquals("labtpo@mail.ru", driver.findElement(By.xpath("(.//*[@class='x-ph__menu__button__text x-ph__menu__button__text_auth'])")).getText());
+    assertTrue(isElementPresent(By.xpath("(.//*[@class='pm-toolbar__button__icon__img  pm-toolbar__button__icon__img_notext pm-toolbar__button__icon__img_avatar pm-toolbar__button__avatar'])")));
   }
 
   @After
   public void tearDown() throws Exception {
+    driver.findElement(By.xpath("(.//*[@class='x-ph__link x-ph__link_last x-ph__auth__link'])")).click();
+
     driver.quit();
     String verificationErrorString = verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
